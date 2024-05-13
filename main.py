c55fa3c31618b394061273fc4fcee793fdce44e5
Пользователь <Dolgin_07@mail.ru>
2024-05-13 18:59:29 +0500

    Города

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import telebot
 import random
 import pyautogui
@@ -6,8 +8,8 @@ import geonamescache
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 bot = telebot.TeleBot(TOKEN)
-
 gc = geonamescache.GeonamesCache()
+all_cities = gc.get_cities()
 
 players = {}
 
@@ -19,8 +21,27 @@ def start_command(message):
 
 @bot.message_handler(content_types=["text"])
 def start_chatting(message):
-    player_city = message.text
-    print(gc.search_cities(player_city, case_sensitive=True, contains_search=True))
+    player_city = message.text.lower()
+
+    pprint(gc.search_cities(player_city, case_sensitive=False, contains_search=True))
+    if not gc.search_cities(player_city, case_sensitive=False, contains_search=True):
+        bot.send_message(message.from_user.id, "Такого города нет")
+        return
+    first_let = player_city[-1]
+    if first_let == "ь" or first_let == "ы" or first_let == "ъ":
+        first_let = player_city[-2]
+    bot.send_message(message.from_user.id, f"Мне на {first_let}")
+
+
+def search_city(first_let):
+    for cities in all_cities:
+        bot_city = all_cities[cities]['alternatenames']
+        for c in bot_city:
+            if c and c[0] == first_let.upper():
+                print(c)
+
+
+search_city("ы")
 
 
 bot.infinity_polling(timeout=20)
@@ -31,4 +52,7 @@ bot.infinity_polling(timeout=20)
 зайти с телефона и отправить гео в бота, а он должен просто отпринтовать координаты
 
 2. сделать так, чтобы бот сам отправлял рандомную локацию)
+
+3. Все города, которые хранятся в переменной "c" - сохранять в отдельный список (внутри функции search_city) и затем,
+после того, как алгоритм дойдёт до конца - просто получить случайный город из списка и вернуть его + отправить юзеру
 """
