8c856763fabe55ff453d76cfb38fc23bc333a59a
Пользователь <Dolgin_07@mail.ru>
2024-05-20 18:57:37 +0500

    Города

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ gc = geonamescache.GeonamesCache()
 all_cities = gc.get_cities()
 
 players = {}
+old_cities = []
 
 
 @bot.message_handler(commands=["start", "help"])
@@ -22,37 +23,43 @@ def start_command(message):
 @bot.message_handler(content_types=["text"])
 def start_chatting(message):
     player_city = message.text.lower()
+    pprint(gc.search_cities(player_city, case_sensitive=False, contains_search=False))
 
-    pprint(gc.search_cities(player_city, case_sensitive=False, contains_search=True))
-    if not gc.search_cities(player_city, case_sensitive=False, contains_search=True):
-        bot.send_message(message.from_user.id, "Такого города нет")
+    if not gc.search_cities(player_city, case_sensitive=False, contains_search=False) or player_city in old_cities:
+        bot.send_message(message.from_user.id, "Такого города нет или он уже был")
         return
+    if len(old_cities):
+        pl_first_letter = old_cities[-1][-1]
+        if player_city[0] != pl_first_letter:
+            bot.send_message(message.from_user.id, "Не та буква в начале")
+            return
+    old_cities.append(player_city)
     first_let = player_city[-1]
+
     if first_let == "ь" or first_let == "ы" or first_let == "ъ":
         first_let = player_city[-2]
     bot.send_message(message.from_user.id, f"Мне на {first_let}")
+    bot.send_message(message.from_user.id, search_city(first_let))
+    print(old_cities)
 
 
 def search_city(first_let):
     for cities in all_cities:
         bot_city = all_cities[cities]['alternatenames']
         for c in bot_city:
-            if c and c[0] == first_let.upper():
-                print(c)
-
-
-search_city("ы")
+            if c and c[0] == first_let.upper()[0] and c.lower() not in old_cities:
+                old_cities.append(c.lower())
+                return c
 
 
 bot.infinity_polling(timeout=20)
 
 """
-Загуглить: "как обработать геолокацию в телебот пайтон"
+Загуглить: "как обработать геолокацию в телебот пайтон" (чекнуть видосик на ютубе 5-и минутный)
 Нужно реализовать так:
 зайти с телефона и отправить гео в бота, а он должен просто отпринтовать координаты
 
 2. сделать так, чтобы бот сам отправлял рандомную локацию)
 
-3. Все города, которые хранятся в переменной "c" - сохранять в отдельный список (внутри функции search_city) и затем,
-после того, как алгоритм дойдёт до конца - просто получить случайный город из списка и вернуть его + отправить юзеру
+
 """
